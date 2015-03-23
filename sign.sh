72bdbecdae8576dc901f95dd51f045894f59d242
Craig R Webster <craig@barkingiguana.com>
2015-03-23 09:34:20 +0000

    Signing script with verifying as a pre-commit hook.

---
+++ sign.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -e
+
+cp hooks/pre-commit .git/hooks/pre-commit
+GPG="gpg --batch --yes --local-user craig@barkingiguana.com --armor"
+find _posts -type f -name "*.md" -or -name "*.html" | while read FILE; do
+  cp $FILE $FILE.unprocessed
+  $GPG --output $FILE.asc --detach-sig $FILE
+done
+
+cp verification.md verification.md.unprocessed
+$GPG --output verification.md.asc --detach-sig verification.md
