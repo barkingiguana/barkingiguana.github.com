69affc0ed6a90fdb78d5d2e4378c375f6d216b19
Craig Webster <craig.webster@versent.com.au>
2020-03-27 00:06:42 +0800

    A cost saving cost calculator

---
+++ cost-saving/index.js
@@ -0,0 +1,19 @@
+function onLoad() {
+  var rootElement = document.getElementById("calculator")
+  var model = {
+    initialize: function () {
+      this.saving = 3600
+      this.salary = 100000
+      this.hours = 16
+    },
+    update: function () {
+      var costPerDay = this.salary / 261
+      var costPerHour = costPerDay / 8
+      var costForSaving = this.hours * costPerHour
+      this.costForSaving = (Math.ceil(costForSaving/100))*100
+
+      this.timeToRecover = (Math.ceil((costForSaving / this.saving) * 365.25)/100)*100
+    },
+  }
+  new Tangle(rootElement, model)
+}
